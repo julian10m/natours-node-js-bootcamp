a69308dd021533923b0ecc925ef9acb389f6bfe0
julian10m <j.d12f90@gmail.com>
2022-06-09 11:19:22 +0200

    prod vs dev errors handling + handler for mongoose errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,62 @@
+const AppError = require('../utils/appError');
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsDB = (err) => {
+  const value = err.errmsg.match(/(["'])(\\?.)*?\1/).at(0);
+  const message = `Duplicate value: ${value}`;
+  return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = (err) => {
+  const fieldsErrorMsg = Object.values(err.errors)
+    .map((val) => val.message)
+    .join('. ');
+  const message = `Invalid input data. ${fieldsErrorMsg}`;
+  return new AppError(message, 400);
+};
+
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    console.error(err.name, 'ERROR', err);
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went wrong',
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-  next();
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else {
+    let error = Object.create(err);
+    if (error.name === 'CastError') {
+      error = handleCastErrorDB(error);
+    } else if (error.code === 11000) {
+      error = handleDuplicateFieldsDB(error);
+    } else if (error.name === 'ValidationError') {
+      error = handleValidationErrorDB(error);
+    }
+    sendErrorProd(error, res);
+  }
 };

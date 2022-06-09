428aadb3c947b068d69205b953a919d75d8082d5
julian10m <j.d12f90@gmail.com>
2022-06-09 11:30:50 +0200

    handling uncaughtExceptions and unhandledRejections

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', (err) => {
+  console.error(err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config({
   path: './config.env',
 });
@@ -19,6 +24,13 @@ const app = require('./app');
 
 const LISTENING_PORT = +process.env.PORT;
 // console.log(process.env);
-app.listen(LISTENING_PORT, () =>
+const server = app.listen(LISTENING_PORT, () =>
   console.log(`Server running on port ${LISTENING_PORT}...`)
 );
+
+process.on('unhandledRejection', (err) => {
+  console.error(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});

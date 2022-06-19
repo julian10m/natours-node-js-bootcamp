bf3f7d4cd23088f38f783dce8fbdd250c83cb806
julian10m <j.d12f90@gmail.com>
2022-06-19 23:24:20 +0200

    set jwt in cookie + adding multiple middleware to increase security

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const morgan = require('morgan');
 const express = require('express');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+
 const toursRouter = require('./routes/toursRoutes');
 const usersRouter = require('./routes/usersRoutes');
 const AppError = require('./utils/appError');
@@ -7,8 +13,36 @@ const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
 
-if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
-app.use(express.json());
+app.use(helmet());
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+const limiter = rateLimit({
+  max: process.env.MAX_REQS_PER_IP,
+  windowMs: process.env.MAX_REQS_TIME_WINDOW,
+  message:
+    'Too many requests from your source IP address, please try again in an hour!',
+});
+app.use('/api', limiter);
+
+app.use(express.json({ limit: '10kb' }));
+app.use(mongoSanitize());
+app.use(xss());
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  })
+);
+
 app.use(express.static(`${__dirname}/public`));
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();

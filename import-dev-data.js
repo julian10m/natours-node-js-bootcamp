fa6f11a91c8557b8f3fcfa1b0c5dab6037810410
julian10m <j.d12f90@gmail.com>
2022-06-21 21:15:03 +0200

    update import-dev-data.js to also load/delete reviews and users

--- import-dev-data.js
+++ import-dev-data.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const Tour = require('./model/tourModel');
+const Review = require('./model/reviewModel');
+const User = require('./model/userModel');
 
 dotenv.config({
   path: './config.env',
@@ -20,6 +22,8 @@ mongoose
 const deleteData = async () => {
   try {
     await Tour.deleteMany();
+    await User.deleteMany();
+    await Review.deleteMany();
     console.log('Data deleted...');
   } catch (err) {
     console.log(err);
@@ -27,21 +31,30 @@ const deleteData = async () => {
   process.exit();
 };
 
-const importData = async (tours) => {
+const importData = async (tours, users, reviews) => {
   try {
     await Tour.create(tours);
+    await User.create(users, { validateBeforeSave: false });
+    await Review.create(reviews);
     console.log('Data loaded! :)');
   } catch (err) {
     console.log(err);
   }
   process.exit();
 };
+
 const tours = JSON.parse(
   fs.readFileSync('./dev-data/data/tours.json', 'utf-8')
 );
+const users = JSON.parse(
+  fs.readFileSync('./dev-data/data/users.json', 'utf-8')
+);
+const reviews = JSON.parse(
+  fs.readFileSync('./dev-data/data/reviews.json', 'utf-8')
+);
 
 if (process.argv[2] === '--import') {
-  importData(tours);
+  importData(tours, users, reviews);
 } else if (process.argv[2] === '--delete') {
   deleteData();
 }

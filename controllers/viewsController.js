78aa8f525f29ae26363df667fb53f1be333299c6
julian10m <35936197+julian10m@users.noreply.github.com>
2022-08-23 00:06:58 +0200

    implementing tour/:slug endpoint and template

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,13 +1,18 @@
 const Tour = require('../model/tourModel');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
 
 exports.getOverview = catchAsync(async (req, res) => {
     const tours = await Tour.find();
     res.status(200).render('overview', { tours });
 });
 
-exports.getTour = (req, res) => {
+exports.getTour = catchAsync(async (req, res, next) => {
+    const tour = await Tour.findOne({ slug: req.params.slug }).populate({ path: 'reviews', fields: 'review rating user' })
+    if (!tour)
+        return next(new AppError('There is no tour with that name.', 404));
     res.status(200).render('tour', {
-        title: 'The title of my Tour'
+        title: `${tour.name} Tour`,
+        tour
     });
-}
+});

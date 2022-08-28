ed64d04ae35692cea26bbc38ffa4acf0e8406fe5
julian10m <35936197+julian10m@users.noreply.github.com>
2022-08-28 16:21:10 +0200

    adding location popups to the map in the tours view

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,8 +1,35 @@
 const locations = JSON.parse(document.getElementById('map').dataset.locations);
 
-mapboxgl.accessToken = 'TOKEN';
+mapboxgl.accessToken = 'pk.eyJ1IjoianVsaWFuMTBtIiwiYSI6ImNsN2Q1bW80YTBpOWczcHBneGwxazEwOHEifQ.sCjIulnGoGTfvYczmGTA8w';
+
 var map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v11'
+    style: 'mapbox://styles/julian10m/cl7d7orbx000n14pnnuw3q4ox',
+    scrollZoom: false
 });
 
+const bounds = new mapboxgl.LngLatBounds();
+locations.forEach(loc => {
+    const el = document.createElement('div');
+    el.className = 'marker';
+    new mapboxgl.Marker({
+        element: el,
+        anchor: 'bottom'
+    }).setLngLat(loc.coordinates).addTo(map);
+    bounds.extend(loc.coordinates);
+
+    new mapboxgl.Popup({
+        offset: 30,
+        closeOnClick: false,
+        focusAfterOpen: false
+    }).setLngLat(loc.coordinates).setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`).addTo(map);
+});
+
+map.fitBounds(bounds, {
+    padding: {
+        top: 200,
+        bottom: 150,
+        left: 100,
+        right: 100
+    }
+});

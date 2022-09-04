a1eaaad87188e684c2927ff1915f13b16b4591bb
julian10m <35936197+julian10m@users.noreply.github.com>
2022-09-04 21:41:29 +0200

    implemented form to update user via a view but without using our API

--- app.js
+++ app.js
@@ -54,6 +54,7 @@ app.use('/api', limiter);
 
 app.use(express.json({ limit: '10kb' }));
 app.use(cookieParser());
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
 app.use(mongoSanitize());
 app.use(xss());
 app.use(
--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,4 +1,5 @@
 const Tour = require('../model/tourModel');
+const User = require('../model/userModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 
@@ -25,4 +26,18 @@ exports.getAccount = (req, res) => {
         title: 'Your account',
         user: req.user
     })
-}
+};
+
+exports.updateUser = catchAsync(async (req, res, next) => {
+    const updatedUser = await User.findByIdAndUpdate(req.user.id, {
+        name: req.body.name,
+        email: req.body.email
+    }, {
+        new: true,
+        runValidators: true
+    });
+    res.status(200).render('account', {
+        title: 'Your account',
+        user: updatedUser
+    })
+});

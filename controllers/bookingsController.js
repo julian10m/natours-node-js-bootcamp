abf672fc75b1b98ce0e9ece2683376df329ddb2d
julian10m <35936197+julian10m@users.noreply.github.com>
2022-09-18 13:57:54 +0200

    completing API functionality for bookings

--- controllers/bookingsController.js
+++ controllers/bookingsController.js
@@ -2,6 +2,7 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const Tour = require('../model/tourModel');
 const Booking = require('../model/bookingModel');
 const catchAsync = require('../utils/catchAsync');
+const factory = require('./handlerFactory');
 const AppError = require('../utils/appError');
 
 exports.getCheckoutSession = catchAsync(async (req, res, next) => {
@@ -41,3 +42,9 @@ exports.createBookingCheckout = catchAsync(async (req, res, next) => {
     await Booking.create({ tour, user, price });
     res.redirect(req.originalUrl.split('?')[0]);
 });
+
+exports.getAllBookings = factory.getAll(Booking);
+exports.getBookingById = factory.getOne(Booking);
+exports.createBooking = factory.createOne(Booking);
+exports.updateBooking = factory.updateOne(Booking);
+exports.deleteBooking = factory.deleteOne(Booking);
--- routes/bookingsRoutes.js
+++ routes/bookingsRoutes.js
@@ -3,7 +3,19 @@ const bookingsController = require('../controllers/bookingsController');
 const authController = require('../controllers/authController');
 
 const router = express.Router({ mergeParams: true });
+router.use(authController.protect);
 
-router.get('/checkout-session/:tourId', authController.protect, bookingsController.getCheckoutSession);
+router.get('/checkout-session/:tourId', bookingsController.getCheckoutSession);
 
+router.use(authController.restrictTo('admin', 'lead-guide'));
+router
+    .route('/')
+    .get(bookingsController.getAllBookings)
+    .post(bookingsController.createBooking);
+
+router
+    .route('/:id')
+    .get(bookingsController.getBookingById)
+    .patch(bookingsController.updateBooking)
+    .delete(bookingsController.deleteBooking);
 module.exports = router;
